e5e4a56714fb378ba857d1885b4eefc4189cbc31
Jörg Ettl <ettjob15@htlkaindorf.at>
2018-10-10 13:00:11 +0200

    Update

--- Exercise_204_Anlangenverzeichnis/src/GUI.java
+++ Exercise_204_Anlangenverzeichnis/src/GUI.java
@@ -39,8 +39,8 @@ private AnlagenModel model = new AnlagenModel();
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
-        jButton1 = new javax.swing.JButton();
+        comboYear = new javax.swing.JComboBox<>();
+        updateTable = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
         jPanel4 = new javax.swing.JPanel();
@@ -55,16 +55,21 @@ private AnlagenModel model = new AnlagenModel();
         jLabel1.setText("Year");
         jPanel1.add(jLabel1);
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016" }));
-        jPanel1.add(jComboBox1);
-
-        jButton1.setText("Update");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        comboYear.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016" }));
+        comboYear.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                comboYearActionPerformed(evt);
             }
         });
-        jPanel1.add(jButton1);
+        jPanel1.add(comboYear);
+
+        updateTable.setText("Update Table");
+        updateTable.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                updateTableActionPerformed(evt);
+            }
+        });
+        jPanel1.add(updateTable);
         jPanel1.add(jPanel3);
         jPanel1.add(jPanel5);
         jPanel1.add(jPanel4);
@@ -93,9 +98,14 @@ private AnlagenModel model = new AnlagenModel();
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+    private void updateTableActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_updateTableActionPerformed
+        int year = Integer.parseInt((String) comboYear.getSelectedItem());
+        System.out.println(year);
+    }//GEN-LAST:event_updateTableActionPerformed
+
+    private void comboYearActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comboYearActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_comboYearActionPerformed
 
     /**
      * @param args the command line arguments
@@ -133,8 +143,7 @@ private AnlagenModel model = new AnlagenModel();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JComboBox<String> comboYear;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
@@ -143,5 +152,6 @@ private AnlagenModel model = new AnlagenModel();
     private javax.swing.JPanel jPanel5;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
+    private javax.swing.JButton updateTable;
     // End of variables declaration//GEN-END:variables
 }
